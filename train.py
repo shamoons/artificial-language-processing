a319e8ba31bffd9a3a31c25d8eadaed986cd7a3d
Shamoon Siddiqui <shamoon@develop.io>
2019-05-16 17:15:55 -0400

    Spitting out the devices we have

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
+from tensorflow.python.client import device_lib
 from textgenrnn import textgenrnn
+print(device_lib.list_local_devices())
 
 textgen = textgenrnn()
 textgen.train_from_file('data/python.txt', num_epochs=1,
