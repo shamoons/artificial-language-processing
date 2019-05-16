5c644ca1aafb7f23e020f14c36360b2e70fa1fb8
Shamoon Siddiqui <shamoon@develop.io>
2019-05-16 16:44:49 -0400

    Got a trainer up and running

--- Scraper/octoscrape.py
+++ Scraper/octoscrape.py
@@ -14,7 +14,7 @@ class Octoscrape:
 
     def search_repos(self):
         return self.g.search_repositories(
-            query='keras stars:>=10 fork:true language:python').get_page(self.page)
+            query='tensorflow stars:>=10 fork:true language:python').get_page(self.page)
 
     def get_contents(self, repo, file_extension):
         contents = repo.get_contents("")
--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from textgenrnn import textgenrnn
 
 textgen = textgenrnn()
-textgen.train_from_file('data/python.txt', num_epochs=1)
+textgen.train_from_file('data/python.txt', num_epochs=1,
+                        new_model=True, header=False)
 textgen.generate(interactive=True, top_n=5)
