8a4a9b92cf1835ad747158aefa954b03600083c0
Shamoon Siddiqui <shamoon@develop.io>
2019-05-17 13:42:44 -0400

    LD_LIBRARY_PATH update

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ module load python/3.7.0
 module load cuda/10.0
 
 export CUDA_HOME=/usr/local/cuda-10.0
-export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib64:$LD_LIBRARY_PATH
 export PATH=/usr/local/cuda-10.0/bin:$PATH
 . /home1/ss19015/.local/share/virtualenvs/PyOctoscraper-KIv7syK9/bin/activate
 cd /gpfs/gpfs/project1/gr19002-001/shamoon/PyOctoscraper
