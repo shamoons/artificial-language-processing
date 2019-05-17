dddd84102529b6d9d078695f21febf3275ce3ca6
Shamoon Siddiqui <shamoon@develop.io>
2019-05-17 10:37:10 -0400

    Trying word level

--- Scraper/octoscrape.py
+++ Scraper/octoscrape.py
@@ -1,6 +1,7 @@
 import base64
 import os
 import autopep8
+import time
 from dotenv import load_dotenv
 from github import Github
 
@@ -14,24 +15,29 @@ class Octoscrape:
 
     def search_repos(self):
         return self.g.search_repositories(
-            query='machine learning stars:>=100 fork:true language:python').get_page(self.page)
+            query='keras stars:>=500 fork:true language:python').get_page(self.page)
 
     def get_contents(self, repo, file_extension):
-        contents = repo.get_contents("")
-        f = open("data/python.txt", "a+")
-        while len(contents) > 1:
-            file_content = contents.pop(0)
+        try:
+            contents = repo.get_contents("")
 
-            if file_content.type == "dir":
-                contents.extend(repo.get_contents(file_content.path))
-            else:
-                if file_content.path.endswith(file_extension):
-                    decoded_content = base64.b64decode(
-                        file_content.content).decode('utf-8')
-                    decoded_content = self._clean_code(decoded_content)
-                    print(decoded_content)
-                    f.write(decoded_content)
-        f.close()
+            f = open("data/python.txt", "a+")
+            while len(contents) > 1:
+                file_content = contents.pop(0)
+
+                if file_content.type == "dir":
+                    contents.extend(repo.get_contents(file_content.path))
+                else:
+                    if file_content.path.endswith(file_extension):
+                        decoded_content = base64.b64decode(
+                            file_content.content).decode('utf-8')
+                        decoded_content = self._clean_code(decoded_content)
+                        print(decoded_content)
+                        f.write(decoded_content)
+            f.close()
+        except:
+            time.sleep(1)
+            pass
 
     def _clean_code(self, code):
         return autopep8.fix_code(code)
--- train.py
+++ train.py
@@ -3,6 +3,7 @@ from textgenrnn import textgenrnn
 print(device_lib.list_local_devices())
 
 textgen = textgenrnn()
-textgen.train_from_file('data/python.txt', num_epochs=1,
+textgen.train_from_file('data/python.txt', num_epochs=5, word_level=True, rnn_bidirectional=True,
+                        rnn_layers=5, rnn_size=256,
                         new_model=True, header=False)
 textgen.generate(interactive=True, top_n=5)
