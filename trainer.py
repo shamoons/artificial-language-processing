a06265511440cf6c0f5e237e809c9ad2cce126f3
Shamoon Siddiqui <shamoon@develop.io>
2019-05-20 12:11:23 -0400

    Printing device

--- trainer.py
+++ trainer.py
@@ -1,4 +1,9 @@
+import tensorflow
+from tensorflow.python.client import device_lib
+
 from Trainer import trainer
+
+print(device_lib.list_local_devices())
 
 
 t = trainer.Trainer('data/python.txt')
