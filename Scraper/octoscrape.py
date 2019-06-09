c964b95c79409be85aba08aa028a00ecf550914e
Shamoon Siddiqui <shamoon@develop.io>
2019-06-09 16:38:22 -0400

    Search code working

--- Scraper/octoscrape.py
+++ Scraper/octoscrape.py
@@ -8,15 +8,23 @@ from dotenv import load_dotenv
 from github import Github
 from github import enable_console_debug_logging
 
-enable_console_debug_logging()
+# enable_console_debug_logging()
 
 load_dotenv()
 
 
 class Octoscrape:
-    def __init__(self, page=0):
+    def __init__(self, page=0, min_code_size=300):
         self.g = Github(login_or_token=os.environ['GITHUB_TOKEN'], retry=5)
         self.page = page
+        self.MIN_CODE_SIZE = min_code_size
+
+    def _delay(self):
+        if self.g.rate_limiting[0] < 15:
+            delay = 15 / self.g.rate_limiting[0]
+            print("Invoking Sleep: ", delay)
+            time.sleep(delay)
+        return
 
     def search_repos(self):
         return self.g.search_repositories(
@@ -24,13 +32,26 @@ class Octoscrape:
 
     def get_contents(self, repo, file_extension):
         try:
-            query = "extension:py+repo:" + repo.full_name
+            query = "size:>" + str(self.MIN_CODE_SIZE)
             print("\n=======\n")
             print(query)
-            code_files = self.g.search_code(query)
+            f = open("data/python.txt", "a")
+            code_files = self.g.search_code(
+                query=query, extension="py", repo=repo.full_name)
             for code_file in code_files:
                 print(code_file)
-                quit()
+                self._delay()
+                decoded_content = base64.b64decode(
+                    code_file.content).decode('utf-8')
+                decoded_content = self._clean_code(decoded_content)
+                if len(decoded_content) > self.MIN_CODE_SIZE:
+                    print("Code size: ", len(decoded_content))
+                    written_file_content = "# " + repo.full_name + "\n"
+                    written_file_content += "# " + code_file.path + "\n"
+                    written_file_content += decoded_content
+                    written_file_content += "<eos>\n"
+                    f.write(written_file_content)
+            f.close()
 
         except Exception as e:
             print("Error: ", e)
